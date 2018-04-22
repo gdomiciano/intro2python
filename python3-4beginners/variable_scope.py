d703bbcb9a82405d136d51279e086c0043d2c17e
Geisy <geisy@kognity.com>
2018-04-22 14:38:58 +0200

    L13 - variable scope

---
+++ python3-4beginners/variable_scope.py
@@ -0,0 +1,11 @@
+my_name = 'Geisy'
+
+def print_name():
+  # Update the global variable:
+  # global my_name
+  my_name = 'Geiseany'
+  print('the name inside the function is', my_name)
+
+print_name()
+
+print('the name outside the function is', my_name)
