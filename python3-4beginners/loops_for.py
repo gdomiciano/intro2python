07c20f9d65d0a3f737b6dcaa7b56e6aa8da48184
Geisy <geisy@kognity.com>
2018-04-22 13:26:37 +0200

    L9 -  for loops

---
+++ python3-4beginners/loops_for.py
@@ -0,0 +1,24 @@
+ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+
+# for ninja in ninjas:
+#   print(ninja)
+
+# # loop through a section of the list
+# for ninja in ninjas[1:3]:
+#   print(ninja)
+
+for ninja in ninjas:
+  if ninja == 'yoshi':
+    print(f'{ninja} - black belt')
+  else:
+    print(ninja)
+
+#break a loop
+for ninja in ninjas:
+  if ninja == 'yoshi':
+    print(f'{ninja} - black belt')
+    break;
+  else:
+    print(ninja)
+
+
