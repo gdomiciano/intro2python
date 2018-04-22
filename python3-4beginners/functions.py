baea7463beb7356e81ba23610b9555ecd1987ecd
Geisy <geisy@kognity.com>
2018-04-22 14:28:58 +0200

    L12 - function with optional and non apitional parameters, return and pass function as argument

---
+++ python3-4beginners/functions.py
@@ -0,0 +1,24 @@
+# def greet(name, time):
+#   print(f'Good {time} {name}, hope you are well')
+
+# name = input('Enter your name: ')
+# time = input('Enter the time: ')
+
+# greet(name, time)
+
+# #defaul parameters / optional arguments
+# def greet(name = 'Gey', time='morning'):
+#   print(f'Good {time} {name}, hope you are well')
+
+# greet()
+
+def area(radius):
+  return 3.142 * radius * radius
+
+def vol(area, length):
+  print(area * length)
+
+radius = int(input('enter the radius: '))
+length = int(input('enter the length: '))
+
+vol(area(radius), length)
